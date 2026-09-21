3eb93e0c67520fa1429c75e0c31331a8e5c186e7
Abhimanyu Kumar <abhimanyukr3218@gmail.com>
2026-09-22 03:04:37 +0530

    Fix headless UI clicks using JS and navigation waits

--- src/main/java/com/qa/utils/WaitUtils.java
+++ src/main/java/com/qa/utils/WaitUtils.java
@@ -5,8 +5,34 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.*;
 
 public class WaitUtils {
+    private final WebDriver driver;
     private final WebDriverWait wait;
-    public WaitUtils(WebDriver driver) { this.wait = new WebDriverWait(driver, Duration.ofSeconds(10)); }
-    public WebElement visible(By locator) { return wait.until(ExpectedConditions.visibilityOfElementLocated(locator)); }
-    public WebElement clickable(By locator) { return wait.until(ExpectedConditions.elementToBeClickable(locator)); }
+
+    public WaitUtils(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    }
+
+    public WebElement visible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public WebElement clickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public void jsClick(By locator) {
+        WebElement element = visible(locator);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block:'center'});", element);
+        wait.until(d -> element.isDisplayed() && element.isEnabled());
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+    }
+
+    public void urlContains(String value) {
+        wait.until(ExpectedConditions.urlContains(value));
+    }
+
+    public boolean invisible(By locator) {
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
 }
