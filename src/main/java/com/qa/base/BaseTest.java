1f7a8127ab4d021deb97db028a3d798214b4e09c
Abhimanyu Kumar <abhimanyukr3218@gmail.com>
2026-09-22 02:45:28 +0530

    Add src/main/java/com/qa/base/BaseTest.java

---
+++ src/main/java/com/qa/base/BaseTest.java
@@ -0,0 +1,24 @@
+package com.qa.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) driver.quit();
+    }
+}
