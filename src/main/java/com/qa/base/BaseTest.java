85ea3fbd9f8a56eb87ab0915b528a2edbe12f6d1
Abhimanyu Kumar <abhimanyukr3218@gmail.com>
2026-09-22 02:48:13 +0530

    Add headless CI and failure screenshots

--- src/main/java/com/qa/base/BaseTest.java
+++ src/main/java/com/qa/base/BaseTest.java
@@ -1,8 +1,12 @@
 package com.qa.base;
 
+import com.qa.utils.ConfigReader;
+import com.qa.utils.ScreenshotUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,13 +16,20 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        if (Boolean.parseBoolean(System.getProperty("headless", "false"))) {
+            options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920,1080");
+        }
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com/");
+        driver.get(ConfigReader.baseUrl());
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
-        if (driver != null) driver.quit();
+    public void tearDown(ITestResult result) {
+        if (driver != null) {
+            if (!result.isSuccess()) ScreenshotUtils.capture(driver, result.getName());
+            driver.quit();
+        }
     }
 }
