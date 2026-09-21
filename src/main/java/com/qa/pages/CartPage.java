df2beac35e4c37b7ba888875de768fb062f2a0a5
Abhimanyu Kumar <abhimanyukr3218@gmail.com>
2026-09-22 02:58:18 +0530

    Stabilize Selenium navigation with explicit waits

--- src/main/java/com/qa/pages/CartPage.java
+++ src/main/java/com/qa/pages/CartPage.java
@@ -1,14 +1,15 @@
 package com.qa.pages;
 
+import com.qa.utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CartPage {
-    private final WebDriver driver;
+    private final WaitUtils wait;
     private final By item = By.cssSelector(".inventory_item_name");
     private final By checkout = By.id("checkout");
 
-    public CartPage(WebDriver driver) { this.driver = driver; }
-    public String getItemName() { return driver.findElement(item).getText(); }
-    public void checkout() { driver.findElement(checkout).click(); }
+    public CartPage(WebDriver driver) { this.wait = new WaitUtils(driver); }
+    public String getItemName() { return wait.visible(item).getText(); }
+    public void checkout() { wait.clickable(checkout).click(); }
 }
