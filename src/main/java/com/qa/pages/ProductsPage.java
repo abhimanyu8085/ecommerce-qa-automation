9dcb9ea73bb8da126051cc7a9d5abb41b413a6ac
Abhimanyu Kumar <abhimanyukr3218@gmail.com>
2026-09-22 02:54:31 +0530

    Improve product page waits and cart actions

--- src/main/java/com/qa/pages/ProductsPage.java
+++ src/main/java/com/qa/pages/ProductsPage.java
@@ -1,19 +1,37 @@
 package com.qa.pages;
 
+import com.qa.utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ProductsPage {
     private final WebDriver driver;
+    private final WaitUtils wait;
     private final By title = By.cssSelector(".title");
     private final By backpackAdd = By.id("add-to-cart-sauce-labs-backpack");
+    private final By backpackRemove = By.id("remove-sauce-labs-backpack");
     private final By cart = By.cssSelector(".shopping_cart_link");
+    private final By cartBadge = By.cssSelector(".shopping_cart_badge");
     private final By menu = By.id("react-burger-menu-btn");
     private final By logout = By.id("logout_sidebar_link");
 
-    public ProductsPage(WebDriver driver) { this.driver = driver; }
-    public boolean isDisplayed() { return driver.findElement(title).isDisplayed(); }
-    public void addBackpackToCart() { driver.findElement(backpackAdd).click(); }
-    public void openCart() { driver.findElement(cart).click(); }
-    public void logout() { driver.findElement(menu).click(); driver.findElement(logout).click(); }
+    public ProductsPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WaitUtils(driver);
+    }
+
+    public boolean isDisplayed() { return wait.visible(title).isDisplayed(); }
+    public void addBackpackToCart() { wait.clickable(backpackAdd).click(); }
+    public void removeBackpack() { wait.clickable(backpackRemove).click(); }
+    public void openCart() { wait.clickable(cart).click(); }
+    public String getCartCount() { return wait.visible(cartBadge).getText(); }
+
+    public boolean isCartBadgePresent() {
+        return driver.findElements(cartBadge).size() > 0;
+    }
+
+    public void logout() {
+        wait.clickable(menu).click();
+        wait.clickable(logout).click();
+    }
 }
