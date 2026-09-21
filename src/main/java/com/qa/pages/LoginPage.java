1025f731dd12663ce3296188b4d2ec4bf2739b3c
Abhimanyu Kumar <abhimanyukr3218@gmail.com>
2026-09-22 02:58:23 +0530

    Stabilize Selenium navigation with explicit waits

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,22 +1,22 @@
 package com.qa.pages;
 
+import com.qa.utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
-    private final WebDriver driver;
+    private final WaitUtils wait;
     private final By username = By.id("user-name");
     private final By password = By.id("password");
     private final By loginButton = By.id("login-button");
     private final By error = By.cssSelector("[data-test='error']");
 
-    public LoginPage(WebDriver driver) { this.driver = driver; }
+    public LoginPage(WebDriver driver) { this.wait = new WaitUtils(driver); }
 
     public void login(String user, String pass) {
-        driver.findElement(username).sendKeys(user);
-        driver.findElement(password).sendKeys(pass);
-        driver.findElement(loginButton).click();
+        wait.visible(username).sendKeys(user);
+        wait.visible(password).sendKeys(pass);
+        wait.clickable(loginButton).click();
     }
-
-    public String getErrorMessage() { return driver.findElement(error).getText(); }
+    public String getErrorMessage() { return wait.visible(error).getText(); }
 }
